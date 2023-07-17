c7b989dbf3ea47be0b2f259fa4d299d63704f636
thlurte <thlurte@gmail.com>
2023-07-17 11:11:23 +0530

    Integrated MySQL

--- Nexus/Nexus/settings.py
+++ Nexus/Nexus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'Nexus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Nexus',
+        'USER': 'root',
+        'PASSWORD': 'Dragon',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
